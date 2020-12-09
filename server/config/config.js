f5d17394695de55758542dfca814aefa102c713a
hectordav <hectordav@gmail.com>
2020-12-08 20:23:24 -0400

    cambios

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV=='dev'){
 	urlDB='mongodb://localhost:27017/cafe';	
 }else{
-	urlDB='mongodb+srv://hector:Fidelito13__@cluster0.b8fhs.mongodb.net/cafe';
+	urlDB=process.env.MONGO_URI;
 }
 
 process.env.URLDB=urlDB;
